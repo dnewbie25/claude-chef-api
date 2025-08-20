904ad551456a58565d2f63cb3aa893a3e0354236
danielito <danielstmontoya@gmail.com>
2025-08-19 19:47:29 -0700

    Refactor main.py to add CORS middleware and update recipe request handling

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from chef import chef_claude
+
 app = FastAPI()
 
+origins = [
+    "http://localhost.com",
+    "https://localhost.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class RecipeRequest(BaseModel):
+  ingredients: list[str]
+
 @app.post("/recipe")
-async def read_item(item: str):
+async def read_item(item: RecipeRequest):
   return chef_claude(item.ingredients)
